dcf9329c25e49e7ffd67eeb2d83f2348941cf33f
santiago92 <santi.alasia92@gmail.com>
2016-12-08 12:04:16 -0300

    add http logging

--- tanner/graylog.py
+++ tanner/graylog.py
@@ -1,21 +1,29 @@
 
 import requests
 import json
-import logging as log
+import logging
+import http.client as http_client
 
 HOST = "192.168.100.167"
 PORT = "12201"
-URL = "http://:12201/gelf"
+
 
 
 class Graylog:
 
     def __init__(self):
         self.endpoint = "http://"+HOST+":"+PORT+"/gelf"
-        log.info ('init graylog endpoint {0}'.format(self.endpoint))
+        logging.info ('init graylog endpoint {0}'.format(self.endpoint))
+
+    def http_log(arg):
+        http_client.HTTPConnection.debuglevel = 1
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(logging.DEBUG)
+        requests_log.propagate = True
 
     def send_data(self,data):
-    	print('url for post {}, data {}'.format(self.endpoint,data))	
-    	requests.post(self.endpoint, data={'short_message':'test_graylogs'})
-
-
+    	print('url for post {}, data {}'.format(self.endpoint,data))
+        http_log()
+    	requests.post(self.endpoint, json={'short_message':'test_graylogs'})
